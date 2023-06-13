f4853913f6dab68457bff4e4147a55bddbb0f023
shui4 <1078185271@qq.com>
2023-06-14 01:12:36 +0800

    style: 修改命名

--- src/main/java/tla/web/WebApp.java
+++ src/main/java/tla/web/WebApp.java
@@ -2,23 +2,19 @@ package tla.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties.LocaleResolver;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.SpringProperties;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootApplication
-public class App {
+public class WebApp {
 
     public static void main(String[] args) {
         SpringProperties.setFlag("spring.xml.ignore");
         ApplicationContext context = SpringApplication.run(
-            App.class,
+            WebApp.class,
             args
         );
         log.info(
